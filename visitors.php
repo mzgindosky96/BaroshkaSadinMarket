25282ed4a45cf7d7e985cc450ab0e507e3ca1439
Mzgin L Ghazi <158284454+mzgindosky96@users.noreply.github.com>
2025-03-21 19:49:35 +0300

    Delete visitors.php
    Signed-off-by: Mzgin L Ghazi <158284454+mzgindosky96@users.noreply.github.com>

--- visitors.php
+++
@@ -1,6 +0,0 @@
-<?php
-$counter_file = 'visitors.dat';
-$count = file_exists($counter_file) ? (int)file_get_contents($counter_file) + 1 : 1;
-file_put_contents($counter_file, $count);
-echo number_format($count);
-?>
