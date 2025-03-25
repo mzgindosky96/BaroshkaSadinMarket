805ad4d902a888d94c03ccf8388e67629eeb94fd
Mzgin L Ghazi <158284454+mzgindosky96@users.noreply.github.com>
2025-03-25 04:19:26 +0300

    Update fetch_products.php
    Signed-off-by: Mzgin L Ghazi <158284454+mzgindosky96@users.noreply.github.com>

--- fetch_products.php
+++ fetch_products.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "baroshka saadin market";
+$dbname = "baroshka_market";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
